084ee34ca5098ff92ed71869ca6e31928a7c2dfe
Agustín DAngelo <agustindangelo2113@gmail.com>
2021-10-19 19:45:07 -0300

    estado de la partida y analizador estático de codigo

--- ahorcado.py
+++ ahorcado.py
@@ -7,13 +7,16 @@ class Ahorcado:
         self.cantidad_aciertos = 0
         self.cantidad_errores = 0
         self.cantidad_errores_permitidos = 6
-        self.estado = 0 # 0: sigue jugando, 1: ganó, -1: perdió.
+        self.codigo_estado = 0 # 0: sigue jugando, 1: ganó, -1: perdió.
+        self.estado = []
+        self.letras_erroneas = []
 
     def get_nombre(self):
         return self.nombre
 
     def set_palabra(self, palabra):
         self.palabra = palabra.lower()
+        self.estado = ['_' for _ in range(len(self.palabra))]
 
     def login(self, nombre):
         if len(nombre) == 0:
@@ -28,15 +31,22 @@ class Ahorcado:
     def get_aciertos_errores(self):
         return self.cantidad_aciertos, self.cantidad_errores
 
-    def check_estado(self):
+    def check_codigo_estado(self):
         if self.cantidad_errores == self.cantidad_errores_permitidos:
-            self.estado = -1
+            self.codigo_estado = -1
         if self.cantidad_aciertos == len(self.palabra):
-            self.estado = 1
-        return self.estado
+            self.codigo_estado = 1
+        return self.codigo_estado
+
+    def get_estado(self):
+        return ' '.join(self.estado)
+
+    def get_letras_erroneas(self):
+        return ' '.join(self.letras_erroneas)
 
     def arriesgar_una_palabra(self, palabra):
         if palabra == self.palabra:
+            self.estado = self.palabra
             return True
         else:
             return False
@@ -47,6 +57,12 @@ class Ahorcado:
             if letra not in self.letras_adivinadas:
                 self.letras_adivinadas.append(letra)
                 self.cantidad_aciertos += self.palabra.count(letra)
+
+                for i, letra_valida in enumerate(self.palabra):
+                    if letra == letra_valida:
+                        self.estado[i] = letra
+
             return True
         self.cantidad_errores += 1
+        self.letras_erroneas.append(letra)
         return False
