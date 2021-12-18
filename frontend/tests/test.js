a69b56ee2b8550672f4cf52f6c105fcb7b92f41b
Agustín D'Angelo <dangeloagustinariel@gmail.com>
2021-12-18 18:32:29 -0300

    incorporando tests de sebas

--- frontend/tests/test.js
+++ frontend/tests/test.js
@@ -52,6 +52,137 @@ describe('LoginTests', function() {
   after(async () => driver.quit());
 })
 
-describe('Partidas', function() {
-  // Testear partidas enteras
-})
+describe('WordToGuessTest', function() {
+
+  const driver = new Builder().forBrowser('firefox').build();
+
+  beforeEach(async () => {
+    await driver.get('http://localhost:4200/newgame');
+    await driver.sleep(1000);
+  });
+
+  it('should throw few characters error', async () => {
+    const word = 'Ag'
+    let input = await driver.findElement(By.id('wordToGuessInput'));
+    await input.clear();
+    await input.click();
+    await input.sendKeys(word);
+    let boton = await driver.findElement(By.id('playButton'));
+    await boton.click();
+
+
+    const errorMessage = await driver.findElement(By.xpath('/html/body/app-root/app-new-game/div/p')).getText();
+    expect(errorMessage).to.equal('La palabra debe tener al menos 3 caracteres.');
+  });
+
+  it('should throw many characters error', async () => {
+    const word = 'Agilessssssssssssssssssssssssssssssssssssss'
+    let input = await driver.findElement(By.id('wordToGuessInput'));
+    await input.clear();
+    await input.click();
+    await input.sendKeys(word);
+    let boton = await driver.findElement(By.id('playButton'));
+    await boton.click();
+
+
+    const errorMessage = await driver.findElement(By.xpath('/html/body/app-root/app-new-game/div/p')).getText();
+    expect(errorMessage).to.equal('La palabra no debe tener más de 20 caracteres.');
+  });
+
+  it('should throw many characters error', async () => {
+    const word = 'Agiles-'
+    let input = await driver.findElement(By.id('wordToGuessInput'));
+    await input.clear();
+    await input.click();
+    await input.sendKeys(word);
+    let boton = await driver.findElement(By.id('playButton'));
+    await boton.click();
+
+
+    const errorMessage = await driver.findElement(By.xpath('/html/body/app-root/app-new-game/div/p')).getText();
+    expect(errorMessage).to.equal('La palabra no debe contener símbolos.');
+  });
+
+  after(async () => driver.quit());
+});
+
+
+describe('GameTest', function() {
+
+  const driver = new Builder().forBrowser('firefox').build();
+
+  beforeEach(async () => {
+    await driver.get('http://localhost:4200/newgame');
+    await driver.sleep(1000);
+
+    const word = 'Agiles'
+    let input = await driver.findElement(By.id('wordToGuessInput'));
+    await input.clear();
+    await input.click();
+    await input.sendKeys(word);
+    let boton = await driver.findElement(By.id('playButton'));
+    await boton.click();
+  });
+
+  it('should throw win game', async () => {
+    const word = 'Agiles'
+    let input = await driver.findElement(By.id('arriesgarInput'));
+    await input.clear();
+    await input.click();
+    await input.sendKeys(word);
+    let boton = await driver.findElement(By.id('arriesgarButton'));
+    await boton.click();
+
+    await driver.sleep(1000);
+
+    const winGameMassage = await driver.findElement(By.id('resultado')).getText();
+    expect(winGameMassage).to.equal('🔥🔥Felicitaciones, ha ganado la partida.');
+  });
+
+  it('should throw game over', async () => {
+    const word = 'Agile'
+    let input = await driver.findElement(By.id('arriesgarInput'));
+    await input.clear();
+    await input.click();
+    await input.sendKeys(word);
+    let boton = await driver.findElement(By.id('arriesgarButton'));
+    await boton.click();
+
+    await driver.sleep(1000);
+
+    const GameOverMassage = await driver.findElement(By.id('resultado')).getText();
+    expect(GameOverMassage).to.equal('😢Ha perdido la partida.');
+  });
+
+  it('should keep playig with error', async () => {
+    const letter = 'u'
+    let input = await driver.findElement(By.id('arriesgarInput'));
+    await input.clear();
+    await input.click();
+    await input.sendKeys(letter);
+    let boton = await driver.findElement(By.id('arriesgarButton'));
+    await boton.click();
+
+    await driver.sleep(1000);
+
+    const GameOverMassage = await driver.findElement(By.id('cantidad-errores')).getText();
+    expect(GameOverMassage).to.equal('❌ Errores: 1');
+  });
+
+  it('should keep playig with ', async () => {
+    const letter = 'a'
+    let input = await driver.findElement(By.id('arriesgarInput'));
+    await input.clear();
+    await input.click();
+    await input.sendKeys(letter);
+    let boton = await driver.findElement(By.id('arriesgarButton'));
+    await boton.click();
+
+    await driver.sleep(1000);
+
+    const GameOverMassage = await driver.findElement(By.id('cantidad-aciertos')).getText();
+    expect(GameOverMassage).to.equal('✅ Letras acertadas: 1');
+  });
+
+  after(async () => driver.quit());
+});
